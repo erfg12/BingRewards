a6b1bbe4c2e11fb30eead17a95ac38fa4f79b9d0
erfg12 <eqrulez@msn.com>
2014-08-12 14:44:20 -0400

    added searchtype to settings.ini
    Also made code more crossplatform friendly.

--- bingRewards/Form1.cs
+++ bingRewards/Form1.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Windows.Forms;
 using System.IO;
 using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Collections;
+using System.Globalization;
 
 namespace WindowsFormsApplication1
 {
@@ -19,20 +22,10 @@ namespace WindowsFormsApplication1
         private int accountNum = 0;
         private bool mobile = false; //start with desktop
         private const int INTERNET_OPTION_END_BROWSER_SESSION = 42;
-        private string settingsFile = Application.StartupPath + @"\settings.ini";
-        private string wordsFile = Application.StartupPath + @"\words.txt";
-        private string accountsFile = Application.StartupPath + @"\accounts.txt";
-
-        [DllImport("wininet.dll", SetLastError = true)]
-        private static extern bool InternetSetOption(IntPtr hInternet, int dwOption, IntPtr lpBuffer, int lpdwBufferLength);
-
-        [DllImport("kernel32")]
-        private static extern int GetPrivateProfileString(string section,
-          string key, string def, StringBuilder retVal, int size, string filePath);
-
-        [DllImport("kernel32")]
-        private static extern int GetPrivateProfileSection(string section, IntPtr lpReturnedString,
-          int nSize, string lpFileName);
+        private string settingsFile = Application.StartupPath + Path.DirectorySeparatorChar + "settings.ini";
+        private string wordsFile = Application.StartupPath + Path.DirectorySeparatorChar + "words.txt";
+        private string accountsFile = Application.StartupPath + Path.DirectorySeparatorChar + "accounts.txt";
+        MultiplatformIni iniSettings;
 
         public Form1()
         {
@@ -82,10 +75,8 @@ namespace WindowsFormsApplication1
 
         public string ReadSettings(string section, string key)
         {
-            const int bufferSize = 255;
-            StringBuilder temp = new StringBuilder(bufferSize);
-            GetPrivateProfileString(section, key, "", temp, bufferSize, settingsFile);
-            return temp.ToString();
+            iniSettings = new MultiplatformIni(settingsFile);
+            return iniSettings.ReadString(section, key, "0");
         }
 
         private int randomNumber()
@@ -143,17 +134,10 @@ namespace WindowsFormsApplication1
             }
         }
 
-        private void clearCookies()
-        {
-            string[] theCookies = System.IO.Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.Cookies));
-            foreach (string currentFile in theCookies)
-                System.IO.File.Delete(currentFile);
-            InternetSetOption(IntPtr.Zero, INTERNET_OPTION_END_BROWSER_SESSION, IntPtr.Zero, 0);
-        }
-
         private void search(Boolean skip = false)
         {
             string query = GetRandomSentence(randomNumber());
+            string searchURL = "http://bing.com/search?q=";
 
             if (webBrowser1.Url.ToString().Contains(@"newagesoldier.com"))
                 return;
@@ -176,14 +160,25 @@ namespace WindowsFormsApplication1
                 }
             }
 
+            if (ReadSettings("settings", "searchtype").Contains("image"))
+                searchURL = "http://www.bing.com/images/search?q=";
+            else if (ReadSettings("settings", "searchtype").Contains("video"))
+                searchURL = "http://www.bing.com/videos/search?q=";
+            else if (ReadSettings("settings", "searchtype").Contains("map"))
+                searchURL = "http://www.bing.com/maps/default.aspx?q=";
+            else if (ReadSettings("settings", "searchtype").Contains("news"))
+                searchURL = "http://www.bing.com/news/search?q=";
+            else if (ReadSettings("settings", "searchtype").Contains("explore") || ReadSettings("settings", "searchtype").Contains("more"))
+                searchURL = "http://www.bing.com/explore?q=";
+
             if (mobile)
             {
-                webBrowser1.Navigate("http://bing.com/search?q=" + query, null, null, "User-Agent: Mozilla/5.0 (Linux; U; Android 4.0.3; ko-kr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30");
+                webBrowser1.Navigate(searchURL + query, null, null, "User-Agent: Mozilla/5.0 (Linux; U; Android 4.0.3; ko-kr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30");
                 if (countDown == 1) //We're on our last search. Reset to desktop.
                     mobile = false;
             } 
             else
-                webBrowser1.Navigate(new Uri("http://bing.com/search?q=" + query));
+                webBrowser1.Navigate(new Uri(searchURL + query));
 
             if (webBrowser1.Url.ToString().Contains(@"?q="))
                 countDown = countDown - 1;
@@ -266,6 +261,228 @@ namespace WindowsFormsApplication1
         private void closeTimer_Tick(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+    }
+
+    // END OF MY CODE
+
+    class MultiplatformIni
+    {
+        private String iniFilePath;
+        public String IniPath
+        {
+            get { return iniFilePath; }
+        }
+        private static Char decSep = NumberFormatInfo.CurrentInfo.NumberDecimalSeparator.ToCharArray()[0];
+        private static Regex keyValRegex = new Regex(
+          @"((\s)*(?<Key>([^\=^\n]+))[\s^\n]*\=(\s)*(?<Value>([^\n]+(\n){0,1})))",
+            RegexOptions.IgnorePatternWhitespace | RegexOptions.Compiled |
+            RegexOptions.CultureInvariant
+        );
+        private static Regex secRegex = new Regex(
+          @"(\[)*(?<Section>([^\]^\n]+))(\])",
+          RegexOptions.Compiled | RegexOptions.CultureInvariant
+        );
+
+        private ArrayList newValues = new ArrayList();
+        struct sectKeyVal
+        {
+            public String section, key, value;
+            public sectKeyVal(String section, String key, String value)
+            {
+                this.section = section;
+                this.key = key;
+                this.value = value;
+            }
+        }
+
+        public MultiplatformIni(String iniFilePath)
+        {
+            this.iniFilePath = iniFilePath;
+        }
+        ~MultiplatformIni()
+        {
+            Flush();
+        }
+
+        private bool isThisTheSection(String line, ref String sectionName)
+        {
+            Match match = secRegex.Match(line);
+            if (match.Success)
+            {
+                String currSection = match.Groups["Section"].Value as String;
+                return (currSection != null && currSection.CompareTo(sectionName) == 0);
+            }
+            return false;
+        }
+        private bool isThisTheKey(String line, ref String key, ref String value)
+        {
+            Match match = keyValRegex.Match(line);
+            if (match.Success)
+            {
+                String currKey = match.Groups["Key"].Value as String;
+                if (currKey != null && currKey.CompareTo(key) == 0)
+                {
+                    value = match.Groups["Value"].Value as String;
+                    return true;
+                }
+            }
+            else if (secRegex.Match(line).Success)
+                value = "Section"; // Oops, we're at the next section
+            return false;
+        }
+
+        public String ReadString(String section, String key, String defaultValue)
+        {
+            FileStream fileStream = new FileStream(iniFilePath, FileMode.OpenOrCreate, FileAccess.Read);
+            TextReader reader = new StreamReader(fileStream);
+            bool sectionFound = false;
+
+            String line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                /* First we find our section,
+                   then - the key */
+                if (!sectionFound)
+                    sectionFound = isThisTheSection(line, ref section);
+                else
+                {
+                    String value = "";
+                    if (isThisTheKey(line, ref key, ref value))
+                    {
+                        fileStream.Close();
+                        return value;
+                    }
+                }
+            }
+
+            fileStream.Close();
+            return defaultValue;
+        }
+        public int ReadInt(String section, String key, int defaultValue)
+        {
+            return Int32.Parse(ReadString(section, key, defaultValue.ToString()));
+        }
+        public Double ReadDouble(String section, String key, Double defaultValue)
+        {
+            return Double.Parse(
+                ReadString(section, key, defaultValue.ToString()).Replace('.', decSep).Replace(',', decSep)
+            );
+        }
+        public void WriteString(String section, String key, String value)
+        {
+            int sameKeyNum = -1;
+            for (int i = 0; i < newValues.Count; i++)
+            {
+                sectKeyVal s = (sectKeyVal)newValues[i];
+                if ((s.section == section) && (s.key == key))
+                    sameKeyNum = i;
+            }
+            if (sameKeyNum != -1)
+                newValues.RemoveAt(sameKeyNum);
+
+            newValues.Add(new sectKeyVal(section, key, value));
+        }
+        public void Flush()
+        {
+            /* This is where all the magic is done
+               We read our INI file, parse the
+               new values and add them to the file
+               before we write it */
+
+            // First we read our file
+            FileStream fileStream = new FileStream(iniFilePath, FileMode.OpenOrCreate, FileAccess.Read);
+            TextReader reader = new StreamReader(fileStream);
+
+            String[] lines = reader.ReadToEnd().Replace('\r', '\0').Split('\n');
+            fileStream.Close();
+
+            // Create our work list and fill it
+            List<String> linesList = new List<String>();
+            foreach (String s in lines)
+                linesList.Add(s);
+
+            foreach (sectKeyVal s in newValues)
+            {
+                String section = s.section;
+                String key = s.key;
+                String value = s.value;
+
+                bool sectionFound = false, keyFound = false;
+                String keyval = "";
+
+                List<String> tempList = new List<String>();
+                for (int i = 0; i < linesList.Count; i++)
+                {
+                    String line = linesList[i];
+
+                    // Did we find our section yet?
+                    if (!sectionFound)
+                        sectionFound = isThisTheSection(linesList[i], ref section);
+                    else
+                    {
+                        // Did we find our key?
+                        if (isThisTheKey(linesList[i], ref key, ref keyval))
+                        {
+                            if (value != keyval)
+                                line = key + "=" + value;
+                            keyFound = true;
+                        }
+                        else if (keyval == "Section" && !keyFound)
+                        {
+                            /* We found the beginning of 
+                               the next section. */
+                            keyval = "";
+                            keyFound = true;
+
+                            if (linesList[i - 1].Trim() == "")
+                                tempList[tempList.Count - 1] = key + "=" + value;
+                            else
+                                tempList.Add(key + "=" + value);
+                            tempList.Add("");
+                        }
+                        else if (i == linesList.Count - 1 && !keyFound)
+                        {   /* Looks like we're at the end of file
+                               and we found our section */
+                            tempList.Add(key + "=" + value);
+                        }
+                    }
+                    tempList.Add(line.TrimEnd('\0'));
+                }
+                linesList = tempList;
+
+                /* The section or the key weren't found
+                   so we add them */
+                if (!sectionFound && !keyFound)
+                {
+                    if (linesList.Count == 1 && linesList[linesList.Count - 1].Trim() == "")
+                        linesList.RemoveAt(0);
+                    else
+                        linesList.Add("");
+
+                    linesList.Add("[" + section + "]");
+                    linesList.Add(key + "=" + value);
+                }
+            }
+
+            // Finally write our file
+            fileStream = new FileStream(iniFilePath, FileMode.Create, FileAccess.Write);
+            TextWriter writer = new StreamWriter(fileStream);
+
+            for (int i = 0; i < linesList.Count; i++)
+                if (!((i == linesList.Count - 1) && (linesList[i].Trim() == "")))
+                    writer.WriteLine(linesList[i]);
+
+            writer.Flush();
+            fileStream.Close();
+        }
+        public void WriteInt(String section, String key, int value)
+        {
+            WriteString(section, key, value.ToString());
+        }
+        public void WriteDouble(String section, String key, Double value)
+        {
+            WriteString(section, key, value.ToString());
         }
     }
 }
